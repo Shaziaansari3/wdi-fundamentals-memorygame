8b3b180bb4f8c5bdf902d7992311341c8aca643b
Shazia Ansari <shaziaansari@Shazias-MacBook-Pro.local>
2019-09-14 13:23:11 -0700

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,30 @@
+/*console.log("Up and running!");
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+console.log("User flipped " +  cardOne);
+console.log("User flipped " + cardThree);*/
+
+const cards =[ "queen" , "queen" , "king" , "king"];
+const cardsInPlay = [];
+
+var cardOne =  cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log("User flipped queen");
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped King");
+
+if (cardsInPlay.length === 2){
+	if 
+}
+
+
+
+
