93ec0bf7125adacc15c3df703d5d8515bf28f1e1
Shazia Ansari <shaziaansari@Shazias-MacBook-Pro.local>
2019-09-14 17:35:08 -0700

    modified game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -15,14 +15,19 @@ cardsInPlay.push(cardOne);
 
 console.log("User flipped queen");
 
-var cardTwo = cards[2];
+var cardTwo = cards[1];
 
 cardsInPlay.push(cardTwo);
 
 console.log("User flipped King");
 
 if (cardsInPlay.length === 2){
-	if 
+
+	if (cardsInPlay[0]===cardsInPlay[1]){
+		alert("u have won")
+	}
+	else {alert("try again")}
+	
 }
 
 
