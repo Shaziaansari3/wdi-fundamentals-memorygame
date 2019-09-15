4e1868823f6cfd8999eaf3d4bac563ef9831ee07
Shazia Ansari <shaziaansari@Shazias-MacBook-Pro.local>
2019-09-15 15:42:54 -0700

    finalized game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,30 +23,54 @@ const cards =[
 }
 
 ];
-const cardsInPlay = [];
+var cardsInPlay = [];
 
 function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-  alert("You found a match!");
-} else {
-  alert("Sorry, try again.");
-}
-}
-
-function flipCard(cardId){
-
-
-console.log("User flipped " + cards[cardId].rank);
-cardsInPlay.push(cards[cardId].rank);
-console.log(cards[cardId].suit);
-console.log(cards[cardId].cardImage);
-if (cardsInPlay.length === 2) {
-	checkForMatch();
+  		alert("You found a match!");
+	} else {
+  		alert("Sorry, try again.");
+	}
 }
 
+function flipCard(){
+	// this refers to the card that was clicked
+	var cardId = this.getAttribute('data-id')
+	var fronturl = cards[cardId].cardImage
+	this.setAttribute('src' , fronturl)
+
+
+
+
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+}
+
+function createBoard(){
+	var board = document.getElementById('game-board')
+	board.innerHTML = "";
+	cardsInPlay = []
+
+	for (var i=0; i<cards.length; i++){
+		var cardElement = document.createElement('img')
+		cardElement.setAttribute('src','images/back.png')
+		cardElement.setAttribute('data-id',i)
+		cardElement.addEventListener('click' , flipCard)
+
+		document.getElementById('game-board')
+		board.appendChild(cardElement)
+
+	}
+
 }
 
 
-flipCard(0);
-flipCard(2);
+createBoard();
+var reset = document.getElementById("reset");
+reset.addEventListener('click', createBoard);
 
